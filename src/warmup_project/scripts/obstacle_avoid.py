8f629377d604bb31b2da59fccca288f13be13b3b
cfong <cfong355@gmail.com>
2014-09-19 20:34:30 -0400

    Updating wall-following code, adding rudimentary obstacle avoid

---
+++ src/warmup_project/scripts/obstacle_avoid.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+
+# Whee globals!
+pub = None
+
+def range_is_valid(scan_msg):
+	"""Returns True if laser scan message range is within valid range"""
+	if scan_msg >= 0.1 and scan_msg < 7.0:
+		return True
+
+	return False
+
+def scan_recieved(msg):
+	"""Call back function for LaserScan messages"""
+	global pub 
+
+	# Default behavior
+	pub.publish(Twist(linear=Vector3(x=0.5)))
+
+	front_scan = [msg.ranges[x] for x in range(0,10) if range_is_valid(msg.ranges[x])]
+
+	# Scans front left and right 'corners' to see if there is stuff in the way
+	right_scan = [msg.ranges[x] for x in range(315-20, 315+20) if range_is_valid(msg.ranges[x])]
+	left_scan = [msg.ranges[x] for x in range(135-20, 135+20) if range_is_valid(msg.ranges[x])]
+
+	right_avg = sum(right_scan)/len(right_scan)
+	left_avg = sum(left_scan)/len(left_scan)
+
+	print 'RIGHT ', right_avg
+	print 'LEFT ', left_avg
+
+	if sum(front_scan)/len(front_scan) < 1:
+		print ('Something here!')
+		pub.publish(Twist(linear=Vector3(x=0)))
+
+		if right_avg < left_avg:
+			pub.publish(Twist(angular=Vector3(z=0.2)))
+		else:
+			pub.publish(Twist(angular=Vector2(z=-0.2)))
+
+
+
+
+
+
+def test():
+	"""Main run loop for testing."""
+	global pub
+	# Initiates as 'wall_follow' node
+	rospy.init_node('obstacle_avoid', anonymous=True)
+
+	# Sets to publish to 'cmd_vel' topic and subscribe to laserscan rangefinder ('scan') topic
+	pub = rospy.Publisher('/cmd_vel', Twist)
+	sub = rospy.Subscriber('/scan', LaserScan, scan_recieved)
+
+	# Sets publish rate to 10 hz 
+	r = rospy.Rate(10)
+
+	while not rospy.is_shutdown():
+		r.sleep()
+
+
+if __name__ == '__main__':
+	try:
+		test()
+
+	except rospy.ROSInterruptException: pass
--- src/warmup_project/scripts/wall_follow.py
+++ src/warmup_project/scripts/wall_follow.py
@@ -1,18 +1,71 @@
 #!/usr/bin/env python
 
-import rospy
+import rospy, math
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
 # Whee globals!
 pub = None
+aligned_to_wall = 'Not aligned'
 
 # Sets default target distance to wall (in meters)
 target_dist = 1.0	
+dist_to_wall = -1.0
 
-def wall_nearby():
-	"""Determine if there is a wall within nearby range. Return 'LEFT', 'RIGHT', 'FRONT', 'BACK' (with respect to robot)"""
-	wall_range = 2;
+def approach_wall(msg):
+	"""Approach wall, ending at specified target distance"""
+	global target_dist, dist_to_wall
+	global pub
+	
+	front_scan = [msg.ranges[x] for x in range(5) if range_is_valid(msg.ranges[x])]
+
+	print dist_to_wall
+	if len(temp) > 0:
+		dist_to_wall = sum(front_scan)/len(front_scan)
+		pub.publish(Twist(linear=Vector3(x=0.3*(dist_to_wall-target_dist))))
+	else:
+		dist_to_wall = -1.0
+
+
+def wall_nearby(msg):
+	"""Determine if there is a wall within nearby range. If so, determine nearest side (LEFT or RIGHT) and orient towards."""
+	global aligned_to_wall
+	global pub 
+
+	# Sketchy stuff to isolate bad values without destroying indices -- to be done properly later  
+	tol = 0.1 # Because scan data is not entirely accurate or consistent
+
+	# Finds shortest laserscan range detected, and assumes it leads to wall
+	full_data = [msg.ranges[x] if range_is_valid(msg.ranges[x]) else 1e3 for x in range(360)]
+	min_dist = min(full_data)
+	theta = full_data.index(min_dist)
+
+	# Constructs a second line 5 degrees off, checks to see if it forms proper right triangle
+	dist_hyp = msg.ranges[(theta+5)%360]
+	dist_opp = math.tan(math.radians(5)/min_dist)
+	if abs(math.sqrt(min_dist**2 + dist_opp**2) - dist_hyp) <= tol:
+		print 'Close enough -- wall detected'
+	else:
+		print 'False alarm'
+
+	print theta
+
+	# Rotates robot, so that nearest side (90=left, 270=right) is aligned with wall (represented by theta)
+	if theta in range(85,95):
+		print 'Basically aligned to left'
+		aligned_to_wall = 'LEFT' 
+	elif theta in range(265,275):
+		print 'Basically aligned to right'
+		aligned_to_wall = 'RIGHT'
+	else:
+		if (theta>180):
+			print 'Turning right'
+			pub.publish(Twist(angular=Vector3(z=-0.2)))
+		else:
+			print 'Turning left'
+			pub.publish(Twist(angular=Vector3(z=0.2)))
+
+	print '***************************************************'
 
 def follow(wall_dir, msg):
 	"""Implement wall-following behavior (parallel to wall) for specified wall direction"""
@@ -23,7 +76,7 @@ def follow(wall_dir, msg):
 
 	# The scanning range that corresponds to each side (assuming +/-45 degrees)
 	scan_ranges = {
-		'LEFT': (range(45, 50), range(130, 135)),
+		'LEFT': (range(45, 60), range(120, 135)),
 		'RIGHT': (range(225, 230), range(310, 315)),
 		'FRONT': (range(315, 320), range(40, 45)),
 		'BACK': (range(135, 140), range(220, 225))
@@ -31,13 +84,12 @@ def follow(wall_dir, msg):
 
 	# Looks at scans from side closest to specified wall direction and calculates difference in scans +/- 45 degrees from presumed center
 	side_front = [msg.ranges[x] for x in scan_ranges[wall_dir][0] if range_is_valid(msg.ranges[x])]
-	side_back = [msg.ranges[x] for x in scan_ranges[wall_dir][1] if range_is_valid(msg.ranges[x])]
+	side_back = [msg.ranges[x] for x in scan_ranges[wall_dir][1] if range_is_valid(msg.ranges[x])]	
+	print('********* SIDE FRONT', side_front, '********')
+	print('********* SIDE BACK', side_back, '********')
 	side_dif = (sum(side_front)/len(side_front)) - (sum(side_back)/len(side_back))
-	print('*********', side_dif, '********')
-	print('*********', side_front, '********')
-	print('*********', side_back, '********')
 
-	If difference in sides is below tolerance, go forward.  Elsewise, turn in logical direction
+	# If difference in sides is below tolerance, go forward.  Elsewise, turn in logical direction
 	if abs(side_dif) <= tol:
 		print ('Close enough -- theoretically moving forward')
 		pub.publish(Twist(linear=Vector3(x=.2)))
@@ -53,7 +105,7 @@ def follow(wall_dir, msg):
 
 def range_is_valid(scan_msg):
 	"""Returns True if laser scan message range is within valid range"""
-	if scan_msg > 0.05 and scan_msg < 7.0:
+	if scan_msg >= 0.1 and scan_msg < 7.0:
 		return True
 
 	return False
@@ -61,6 +113,7 @@ def range_is_valid(scan_msg):
 def scan_recieved(msg):
 	"""Callback function for msg of type sensor_msgs/LaserScan"""
 	global pub 
+	global aligned_to_wall
 
 	base_turn_speed = 4
 	tol = 1.5e-2
@@ -70,7 +123,14 @@ def scan_recieved(msg):
 		return
 
 	# follow(wall_nearby(), msg)
-	follow('LEFT', msg)
+	# follow('LEFT', msg)
+	# approach_wall(msg)
+
+	if aligned_to_wall == 'Not aligned':
+		wall_nearby(msg)
+	else:
+		follow(aligned_to_wall, msg)
+
 
 def test():
 	"""Main run loop for testing."""
